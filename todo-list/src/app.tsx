257e1628e710b343971217b54c4c315c84c62fee
Chere <alexandre2405@gmail.com>
2024-07-16 00:20:39 -0300

    Estilizando adicionando barra de rolagem

--- todo-list/src/app.tsx
+++ todo-list/src/app.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight, Check, Circle, CircleCheck, Pencil, Trash2, X } from "lucide-react"
 import { useRef, useState } from "react"
+import './app.css'
 
 interface Task{
   id: number;
@@ -128,72 +129,77 @@ export function App() {
         </div>
          
         <ul>
-          <div className='space-y-4'>
-            
-            {tasks.map(task => (
-              <li key={task.id}>
-              
-                <div className='h-16 px-4 rounded-xl flex items-center shadow-shape gap-3 bg-orange-400'>
-                  <button onClick={() => TaskCompleted(task.id)} >{task.isDone ? <CircleCheck className="text-green-700"/> : <Circle /> }</button>
-                  {task.isEditing ? (
-                    <input
-                      type="text"
-                      value={task.text}
-                      className="bg-transparent placeholder-zinc-50 text-zinc-50 outline-none"
-                      onChange={(e) => updateTask(task.id, e.target.value)}
-                      placeholder="Edite a tarefa" 
-                      autoFocus
-                    />
-                  ) : (<span> {task.text} </span>)}
-                <div className="ml-auto flex gap-2">
-                  <button onClick={() => {toggleEditTask(task.id)}} className=" ml-auto rounded-lg">{task.isEditing ? <Check className="text-green-700"/> : <Pencil className="size-6"/>}</button>
-                  <button onClick={() => {removeTask(task.id), setConfirmModal(true)}} className=" ml-auto rounded-lg bg-orange-300 hover:bg-red-300">
-                    <X className="text-red-700"/>
-                  </button>
-                </div> 
-                </div>
-              </li>
-
-              
-            ))}
-            
-            {verification && (
-              <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-                <div className="rounded-xl py-6 px-6 shadow-shape bg-zinc-900 space-y-5">
-                    <div className="flex- items-center justify-between">
-                      <h2 className="text-2xl font-semibold mb-6">Tarefa já adicionada!</h2>
-                      <button onClick={closeModal} className="bg-orange-600 py-3 w-full rounded-lg hover:bg-zinc-50 hover:text-orange-600">Okay</button>
-                    </div>
-                </div>
-              </div>
-            )} 
-
-            {confirmModal && (
-              <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-              <div className="max-w-lg rounded-xl py-6 px-5 shadow-shape bg-zinc-900 space-y-5">
-                <h2 className="text-2xl font-semibold mb-6">Tem certeza que deseja excluir essa tarefa?</h2>
-                <div className="flex justify-center space-x-4">
-                  <button onClick={confirmRemoveTask} className="bg-orange-600 py-3 w-1/2 px-6 rounded-lg hover:bg-orange-700">Sim</button>
-                  <button onClick={closeConfirmModal} className="border border-white py-3 px-6 w-1/2 rounded-lg hover:bg-zinc-800">Não</button>
-                </div>
-              </div>
-            </div>
-            )}
-
-            {isTrashModal && (
-              <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-              <div className="max-w-lg rounded-xl py-6 px-5 shadow-shape bg-zinc-900 space-y-5">
-                <h2 className="text-2xl font-semibold mb-6">Você vai apagar toda lista de tarefas, deseja fazer isso?</h2>
-                <div className="flex justify-center space-x-4">
-                  <button onClick={deleteAll} className="bg-orange-600 py-3 w-1/2 px-6 rounded-lg hover:bg-orange-700">Sim</button>
-                  <button onClick={closeTrashModal} className="border border-white py-3 px-6 w-1/2 rounded-lg hover:bg-zinc-800">Não</button>
-                </div>
-              </div>
-            </div>
-            )}
           
-          </div>
+            <div className="max-h-96 overflow-y-auto pr-4">
+              <div className='space-y-4'>
+                
+                {tasks.map(task => (
+                  <li key={task.id}>
+                  
+                    <div className='h-16 px-4 rounded-xl flex items-center shadow-shape gap-3 bg-orange-400'>
+                      <button onClick={() => TaskCompleted(task.id)} >{task.isDone ? <CircleCheck className="text-green-700"/> : <Circle /> }</button>
+                      {task.isEditing ? (
+                        <input
+                          type="text"
+                          value={task.text}
+                          className="bg-transparent placeholder-zinc-50 text-zinc-50 outline-none"
+                          onChange={(e) => updateTask(task.id, e.target.value)}
+                          placeholder="Edite a tarefa" 
+                          autoFocus
+                        />
+                      ) : (<span> {task.text} </span>)}
+                    <div className="ml-auto flex gap-2">
+                      <button onClick={() => {toggleEditTask(task.id)}} className=" ml-auto rounded-lg">{task.isEditing ? <Check className="text-green-700"/> : <Pencil className="size-6"/>}</button>
+                      <button onClick={() => {removeTask(task.id), setConfirmModal(true)}} className=" ml-auto rounded-lg bg-orange-300 hover:bg-red-300">
+                        <X className="text-red-700"/>
+                      </button>
+                    </div> 
+                    </div>
+                  </li>
+
+                  
+                ))}
+                
+                {verification && (
+                  <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+                    <div className="rounded-xl py-6 px-6 shadow-shape bg-zinc-900 space-y-5">
+                        <div className="flex- items-center justify-between">
+                          <h2 className="text-2xl font-semibold mb-6">Tarefa já adicionada!</h2>
+                          <button onClick={closeModal} className="bg-orange-600 py-3 w-full rounded-lg hover:bg-zinc-50 hover:text-orange-600">Okay</button>
+                        </div>
+                    </div>
+                  </div>
+                )} 
+
+                {confirmModal && (
+                  <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+                  <div className="max-w-lg rounded-xl py-6 px-5 shadow-shape bg-zinc-900 space-y-5">
+                    <h2 className="text-2xl font-semibold mb-6">Tem certeza que deseja excluir essa tarefa?</h2>
+                    <div className="flex justify-center space-x-4">
+                      <button onClick={confirmRemoveTask} className="bg-orange-600 py-3 w-1/2 px-6 rounded-lg hover:bg-orange-700">Sim</button>
+                      <button onClick={closeConfirmModal} className="border border-white py-3 px-6 w-1/2 rounded-lg hover:bg-zinc-800">Não</button>
+                    </div>
+                  </div>
+                </div>
+                )}
+
+                {isTrashModal && (
+                  <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+                  <div className="max-w-lg rounded-xl py-6 px-5 shadow-shape bg-zinc-900 space-y-5">
+                    <h2 className="text-2xl font-semibold mb-6">Você vai apagar toda lista de tarefas, deseja fazer isso?</h2>
+                    <div className="flex justify-center space-x-4">
+                      <button onClick={deleteAll} className="bg-orange-600 py-3 w-1/2 px-6 rounded-lg hover:bg-orange-700">Sim</button>
+                      <button onClick={closeTrashModal} className="border border-white py-3 px-6 w-1/2 rounded-lg hover:bg-zinc-800">Não</button>
+                    </div>
+                  </div>
+                </div>
+                )}
+              
+              </div>
+            </div>
+          
         </ul>
+        
       </div>
     </div>
     
